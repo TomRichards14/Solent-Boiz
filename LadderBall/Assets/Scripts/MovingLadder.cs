dbd56f82edad5cf6d62eaef82464b1bd808ca5cf
MrResistance <Paul1208Gush@Gmail.com>
2017-11-06 12:14:18 +0000

    Lets try this again

---
+++ LadderBall/Assets/Scripts/MovingLadder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingLadder : MonoBehaviour {
+    public bool GoingLeft = false;
+    public bool GoingRight = true;
+	void Update () {
+        if (GoingLeft == true)
+        {
+            GoLeft();
+        }
+        if (GoingRight == true)
+        {
+            GoRight();
+        }
+    }
+    //CHANGE DIRECTION WHEN THE PLATFORM COLLIDES WITH THE BARRIERS
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (GoingLeft == true)
+        {
+            GoingLeft = false;
+            GoingRight = true;
+        }
+        else if (GoingRight == true)
+        {
+            GoingRight = false;
+            GoingLeft = true;
+        }
+    }
+    void GoLeft()
+    {
+        transform.Translate(Vector3.right * Time.deltaTime);
+    }
+    void GoRight()
+    {
+        transform.Translate(Vector3.right * -Time.deltaTime);
+    }
+}
